a00acf2cb040d2262e3c216866fc4e5634efa7b9
Ivan Chvets <ivan.chvets@kinarasystems.com>
2026-03-17 12:14:14 -0400

    feat: Added status to internal REST API. Signed-off-by: Ivan Chvets <ivan.chvets@kinarasystems.com>

--- src/RESTAPI/RESTAPI_routers.cpp
+++ src/RESTAPI/RESTAPI_routers.cpp
@@ -26,7 +26,8 @@ namespace OpenWifi {
 	RESTAPI_IntRouter(const std::string &Path, RESTAPIHandler::BindingMap &Bindings,
 					  Poco::Logger &L, RESTAPI_GenericServerAccounting &S, uint64_t TransactionId) {
 		return RESTAPI_Router_I<RESTAPI_operation_handler, RESTAPI_results_handler,
-								RESTAPI_simulation_handler, RESTAPI_system_command>(
+								RESTAPI_simulation_handler, RESTAPI_status_handler,
+								RESTAPI_system_command>(
 			Path, Bindings, L, S, TransactionId);
 	}
 } // namespace OpenWifi
