9abc7c1e847259b2afcc209b5429857f1320651e
Ivan Chvets <ivan.chvets@kinarasystems.com>
2026-03-31 11:16:11 -0400

    fix: Removed trailing 0 from MAC generation. Signed-off-by: Ivan Chvets <ivan.chvets@kinarasystems.com>

--- src/SimulationRunner.cpp
+++ src/SimulationRunner.cpp
@@ -38,7 +38,7 @@ namespace OpenWifi {
         std::uint64_t ReactorIndex=0;
 		for (uint64_t i = 0; i < Details_.devices; i++) {
 			char Buffer[32];
-			snprintf(Buffer, sizeof(Buffer), "%s%05x0", Details_.macPrefix.c_str(), (unsigned int)i);
+			snprintf(Buffer, sizeof(Buffer), "%s%06x", Details_.macPrefix.c_str(), (unsigned int)i);
 			auto Client = std::make_shared<OWLSclient>(Buffer, Logger_, this, *SocketReactorPool_[ReactorIndex++ % NumberOfReactors_]);
             Client->SerialNumber_ = Buffer;
             Client->Valid_ = true;
